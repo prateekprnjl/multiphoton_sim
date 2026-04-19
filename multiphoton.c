bcb405fe6dec0ad3ce453e3b428ef58ec8c7b7c5
ppranjal <180265851+prateekprnjl@users.noreply.github.com>
2026-04-19 17:18:58 +0200

    update

--- multiphoton.c
+++ multiphoton.c
@@ -1,43 +1,66 @@
-// === TDSE Parallel Solver === //
-// === PetSc + MPI implementation === //
+/* TDSE Parallel Solver 
+   PetSc + MPI implementation 
+*/
 
+#include <mpi.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include <petsc.h>
+#include <petscts.h>
+#include <petscvec.h>
+#include <petscmat.h>
 
-#define PI 3.1415926535
 #define ROOT 0
-#define MAX_ITER 1000
+#define PI 3.1415926535
 
-Vec psi;
-Vec E;
-Mat A;
-double omega;
-int rank;
+Vec E;			/* Energy vector */
+Mat A; 			/* Interaction matrix */
+Vec psi0;		/* Initial wavefunction	*/
+Vec psi;		/* Working wavefunction	*/
+int rank, size;		/* MPI */
 
+/* Laser parameters */
 typedef struct{
-	double amplitude;
 	double omega;
+	double amplitude;
+
 	double t_ini;
 	double t_end;
 } LaserPulse;
 
-void initPETSc();
-void initialize();
-void loadInitialState(int vec);
-void buildHamiltonian();
-double externalPotential(double t);
-void solveTDSE();
-void writeOutput();
+/* Pump-Probe field setup */
+typedef struct{
+	LaserPulse pump;
+	LaserPulse probe;
+
+	double delay;
+	double t_final;
+} SimulationParameters;
+
+void initPETSc(int argc,char **argv);
+void loadMatrix(void);
+void loadInitialState(void);
+
+void solveTDSE(MPI_Comm subcomm, SimulationParameters job);
+PetscErrorCode RHSFunction(TS ts,PetscReal t,Vec u,Vec f,void *ctx);
+double externalPotential(double t, SimulationParameters *job);
+
+void writeOutput(MPI_Comm subcomm,int delayIndex);
+void cleanup(void);
 
 int main(){
-	initPETSc();
-	loadHamiltonian();
-	loadInitialState(vec);
-	buildHamiltonian();
-	solveTDSE();
-	writeOutput();
+	int cpusPerJob;
+	initPETSc(argc,argv);
+	parseInputs(argc,argv,&cpusPerJob);
+	loadMatrix();
+	loadInitialState();
+
 	cleanup();
+	PetscFinalize();
+
+	return 0;
 }
 
